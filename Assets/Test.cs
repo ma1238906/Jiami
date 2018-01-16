1da8bfa326a7ed839b0d9e77765366cb9a024116
Ma Junpeng <373834486@qq.com>
2018-01-16 17:25:02 +0800

    add test.cs for test action

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Action[] actions = new Action[10];
+    void Start ()
+    {
+        
+		for(int i=0;i<10;i++)
+		{
+		    int j = i;
+		    actions[i] = () => Debug.Log(j);
+		}
+        
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            foreach (Action action in actions)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
